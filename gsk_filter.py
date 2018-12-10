8d8d33cf39bf85ee69ced5b583bd4510d8985693
Unknown <marcomunozperez@uma.es>
2018-12-10 10:40:07 +0100

    Updated files

--- gsk_filter.py
+++ gsk_filter.py
@@ -1,3 +1,7 @@
+import json
+
+from conda._vendor.tqdm import tqdm
+
 from chembl_webresource_client.new_client import new_client
 
 assay = new_client.assay
@@ -15,9 +19,24 @@ print('Documents found in search: ' + str(doc_list))
 print('Filtering activities with doc id in previous query...')
 
 res = activity.filter(document_chembl_id__in=doc_list)
-print('Number of Bioactivity Search Results: '+str(len(res)))
-
+print('Number of Bioactivity Search Results: ' + str(len(res)))
+input()
 print(res[0])
 
-for i in res[0]:
-    print(i)
+with open('interactions.json', 'w') as file:
+    for interaction in tqdm(res):
+        file.write(interaction['target_chembl_id']+'\n')
+
+# chembl_target_ids = set()
+
+# for interaction in tqdm(res):
+#    chembl_target_ids.add(interaction['target_chembl_id'])
+
+# print(chembl_target_ids)
+
+activities = new_client.target
+res2 = activities.filter(target_chembl_id="CHEMBL4367",
+                         pchembl_value__isnull=False)
+
+for e in res2:
+    print(e)
