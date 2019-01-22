9dd25f880cfe9816498a3b86ecbac83bb18a9ac9
Miguel González <sosa@uma.es>
2019-01-22 13:16:59 +0100

    Minor fixes.

--- workflow.sh
+++ workflow.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-# python python/gsk_filter.py | sort -u > outputs/interactions.tsv
+python python/gsk_filter.py | sort -u > outputs/interactions.tsv
 
 cut -f 1 < outputs/interactions.tsv | sort -u > outputs/unique_targets_chembl.tsv
 
